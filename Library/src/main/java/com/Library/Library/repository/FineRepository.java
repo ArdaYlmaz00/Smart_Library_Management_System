ac0971caefc95af46c84250075dffd890c59c2cd
ArdaYlmaz00 <ard1ylmaz0@gmail.com>
2025-12-14 13:15:07 +0300

    Değişiklikler
    Backend tarafı bitti frontend hala geliştirlilebilir

--- Library/src/main/java/com/Library/Library/repository/FineRepository.java
+++
@@ -1,6 +0,0 @@
-package com.Library.Library.repository;
-import com.Library.Library.entity.Fine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FineRepository extends JpaRepository<Fine, Long> {}
--- Library/src/main/java/com/Library/Library/repository/StaffRepository.java
+++
@@ -1,6 +0,0 @@
-package com.Library.Library.repository;
-import com.Library.Library.entity.Staff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface StaffRepository extends JpaRepository<Staff, Long> {}
